0ec389500a67b6c9cb6f565a63dae3c3b6f28827
David Gasinski <dgasinski1@sheffield.ac.uk>
2023-10-26 16:05:36 +0100

    added lib to generator pie charts in svg

--- digest.py
+++ digest.py
@@ -1,17 +1,22 @@
 import requests
 from flask import Flask, Response
 from markupsafe import escape
+from lib.chart import generateSVGChart
 
 svg_filepath = "static/digest.svg"
+svg_chart = "static/components/chart.svg"
 with open (svg_filepath, 'r') as f:
     svg_file = f.read()
+with open (svg_chart, 'r') as c:
+    svg_chart_file = c.read()
 
 app = Flask(__name__)
 
 def populateSVG(svg: str, username: str):
     req = requests.get(f'https://leetcode-stats-api.herokuapp.com/{username}')
     req_json = req.json()
-    return svg.format(easy=req_json["easySolved"], medium=req_json["mediumSolved"], hard=req_json["hardSolved"], rank=req_json["ranking"])
+    chart_file = generateSVGChart(svg_chart_file, 270, '#000000', '#FFFFFF')
+    return svg.format(easy=req_json["easySolved"], medium=req_json["mediumSolved"], hard=req_json["hardSolved"], rank=req_json["ranking"], chart=chart_file)
 
 @app.route('/svg/<username>')
 def svg(username):
